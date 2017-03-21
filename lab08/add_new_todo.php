f765fb4eedcbc0400534b40839a60f2054d07939
Sontaya58160434 <58160434@go.buu.ac.th>
2017-03-22 02:47:26 +0700

    update

--- lab08/add_new_todo.php
+++ lab08/add_new_todo.php
@@ -1,11 +1,26 @@
 <?php
 $link = mysql_connect('localhost', 'it58160434', '0896959711');
 mysql_query("SET NAMES UTF8");
-mysql_select_db('it58160434' $link);
+mysql_select_db('it58160434', $link);
 
 // insert new todo
 $start = date('Y-m-d H:i:s');
 $topic = addslashes($_POST['topic']);
 $sql = "INSERT INTO todo (start, topic, status) VALUES ('$start', '$topic', 0)";
 
+mysql_query($sql);
+
+// get data back
+$sql = "SELECT *
+        FROM todo
+        ORDER BY status, id desc
+        ";
+$result = mysql_query($sql, $link);
+$html = "<ol>";
+while($obj = mysql_fetch_object($result)){
+    $html.="<li>".$obj->topic." ";
+    $html.="<font color='#009900' size='1'>[".$obj->start."]</font>";
+}
+$htnl.="</ol>";
+echo $html;
 ?>
